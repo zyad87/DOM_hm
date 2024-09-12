b9b460f9fe92e9956b637a89414fd293af61eaf4
zyad87 <zyadt871@gmail.com>
2024-09-12 09:09:52 +0300

    first commit

---
+++ min.js
@@ -0,0 +1,80 @@
+function toggleVisibility(elementId1, elementId2) {
+    const element1 = document.getElementById(elementId1);
+    const element2 = document.getElementById(elementId2);
+    
+    element1.classList.toggle('hidden');
+    element2.classList.toggle('hidden');
+  }
+  
+  function showImage() {
+    toggleVisibility('profileImage', 'profileImage2');
+  }
+  
+  function hideImage() {
+    toggleVisibility('profileImage', 'profileImage2');
+  }
+  
+  function showContact() {
+    toggleVisibility('contact1', 'contact2');
+  }
+  
+  function hideContact() {
+    toggleVisibility('contact1', 'contact2');
+  }
+  
+  function showEducation() {
+    toggleVisibility('education1', 'education2');
+  }
+  
+  function hideEducation() {
+    toggleVisibility('education1', 'education2');
+  }
+  
+  function showSkills() {
+    toggleVisibility('listSkills1', 'listSkills2');
+  }
+  
+  function hideSkills() {
+    toggleVisibility('listSkills1', 'listSkills2');
+  }
+  
+  function showLanguages() {
+    toggleVisibility('languageList1', 'languageList2');
+  }
+  
+  function hideLanguages() {
+    toggleVisibility('languageList1', 'languageList2');
+  }
+  
+  function showName() {
+    toggleVisibility('nameSection1', 'nameSection2');
+  }
+  
+  function hideName() {
+    toggleVisibility('nameSection1', 'nameSection2');
+  }
+  
+  function showProfile() {
+    toggleVisibility('profileContent1', 'profileContent2');
+  }
+  
+  function hideProfile() {
+    toggleVisibility('profileContent1', 'profileContent2');
+  }
+  
+  function showExperience() {
+    toggleVisibility('experience1', 'experience2');
+  }
+  
+  function hideExperience() {
+    toggleVisibility('experience1', 'experience2');
+  }
+  
+  function showRefrence() {
+    toggleVisibility('reference1', 'reference2');
+  }
+  
+  function hideRefrence() {
+    toggleVisibility('reference1', 'reference2');
+  }
+  
